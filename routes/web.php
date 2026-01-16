8a6f7cb4c87d23a2c6696b4b5b1de2ad1e04f927
HoracioValdes <horacio.valdes@hotmail.com>
2026-01-16 12:29:15 -0300

    refactor: use resource routes for notes CRUD

--- routes/web.php
+++ routes/web.php
@@ -7,14 +7,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/notes', [NoteController::class, 'index']);
-
-Route::get('/notes/create', [NoteController::class, 'create']);
-Route::post('/notes', [NoteController::class, 'store']);
-
-Route::get('/notes/{note}', [NoteController::class, 'show']);
-
-Route::get('/notes/{note}/edit', [NoteController::class, 'edit']);
-Route::put('/notes/{note}', [NoteController::class, 'update']);
-
-Route::delete('/notes/{note}', [NoteController::class, 'destroy']);
+Route::resource('notes', NoteController::class);
